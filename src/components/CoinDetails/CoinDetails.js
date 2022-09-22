eb0eac9d6b8998efb10488253126f8fba1c1de80
Rukonuzzaman Shiem <rukonuzzaman.shiem.351@gmail.com>
2022-09-22 14:19:22 +0600

    first commit

---
+++ src/components/CoinDetails/CoinDetails.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Spinner from '../Spinner/Spinner';
+
+const CoinDetails = () => {
+    const { id } = useParams();
+    const [coin, setCoin] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        const url = `https://api.coingecko.com/api/v3/coins/${id}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setCoin(data)
+                setLoading(false);
+            });
+    }, [id]);
+    return (
+        <>
+            {loading ? (<Spinner></Spinner>) : (
+                <div className='px-4 h-[80vh] pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+                    <div className='h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 content-center'>
+                        <div className='order-2 md:order-1'>
+                            <h1 className='text-3xl'>General Info:</h1>
+                            <hr />
+                            <h1>Coin Name: {coin.name}</h1>
+                            <h1>Market Cap Rank: {coin.market_cap_rank}</h1>
+                            <h1>Origin: {coin.country_origin ? coin.country_origin : "Not Found"}</h1>
+
+                            <h1 className='text-3xl'>Scores:</h1>
+                            <hr />
+                            <h1>Community Score: {coin.community_score}</h1>
+                            <h1>Developer Score: {coin.developer_score}</h1>
+                            <h1>Liquidity Score: {coin.liquidity_score}</h1>
+                            <h1>Public Interest Score: {coin.public_interest_score}</h1>
+                        </div>
+                        <div className='flex justify-center items-center order-1 md:order-2'>
+                            <img src={coin.image?.large} alt="coinImg" />
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default CoinDetails;
